6d41b065a3e6c34f855540e17ee362083525da39
Arun <a.v.venugopal@student.utwente.nl>
2023-01-27 15:26:07 +0100

    Added a simple CNN architecure

--- CNN_model.py
+++ CNN_model.py
@@ -5,13 +5,18 @@ from numpy import array
 import rasterio as rio
 from rasterio.plot import reshape_as_image
 import geopandas as gpd
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.layers import MaxPool2D
-from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Dropout
-from tensorflow.keras.layers import Dense
+import tensorflow as tf
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Conv2D
+# from tensorflow.keras.layers import MaxPool2D
+# from tensorflow.keras.layers import Flatten
+# from tensorflow.keras.layers import Dropout
+# from tensorflow.keras.layers import Dense
 
 class CNN_model:
     
@@ -24,6 +29,7 @@ class CNN_model:
         self.ignore_patch_list = list()
         self.x = list()
         self.y = list()
+        self.scaler = StandardScaler()
         
     def read_training(self):
         training_file_list = glob.glob(os.path.join(self.training_path,"*.tif"))
@@ -39,20 +45,70 @@ class CNN_model:
             self.x.append(patch_src_read)
             self.y.append(float(target_gdf.query(f"patch_name == '{f_name}'")["yld_kg_sqm"]))
             patch_src.close()
-        self.x, self.y = np.array(self.x), np.array(self.y)
+        self.y = self.scaler.fit_transform(np.array(self.y).reshape(-1, 1))
+        self.x = np.array(self.x)
+        # print(self.y)
+        self.x = np.nan_to_num(self.x, nan=0)# Check for different value for no data
         print(f"x shape :{self.x.shape}, y shape: {self.y.shape}")
+        print(np.nanmin(self.x),np.nanmax(self.x))
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.x, self.y, test_size=0.25)
         
     def build_CNN(self):
+        ## Set the model architecture here
         
+        # config = tf.ConfigProto()
+        # config.gpu_options.allow_growth = True
+        # sess = tf.Session(config=config)
+        model = models.Sequential()
+        model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=self.patch_dim))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(layers.MaxPooling2D((2, 2)))
         
+        model.add(layers.Flatten())
+        model.add(layers.Dense(64, activation='relu')) # Add another dense layer
+        model.add(layers.Dense(32, activation='relu'))
+        
+        model.add(layers.Dense(1,activation='linear'))
+        model.compile(optimizer='adam',
+                      loss='mse', metrics=['mae'])
+        
+        # early stopping callback
+        es = EarlyStopping(monitor='val_loss',
+                   mode='min',
+                   patience=50,
+                   restore_best_weights = True)
+        
+        history = model.fit(self.X_train, self.y_train,
+                    validation_data = (self.X_test, self.y_test),
+                    callbacks=[es],
+                    epochs=50,
+                    batch_size=8,
+                    verbose=1)
+        model.save('Output/models/CNN/')
+        plt.plot(history.history['loss'], label='loss')
+        plt.plot(history.history['val_loss'], label = 'val_loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Accuracy')
+        plt.ylim([0.5, 1])
+        plt.legend(loc='lower right')
+
+        # test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+        plt.savefig("Output/CNN_loss_acc.png")
+        plt.close()
+    
     def run(self):
         self.read_training()
         self.build_CNN()
         # self.prepare
         # pass
-    
+        # Plot training and val loss
+        # Plot training acc and val acc
+        # Correlation map between avergae(min and max) ndvi,evi and bands for patch and the target
 
+        
 if __name__ == "__main__":
     cnn = CNN_model()
     cnn.run()
