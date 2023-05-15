a9d2bbace7df3e3fd39c00f0eff010bc139254e9
Arun <a.v.venugopal@student.utwente.nl>
2023-05-15 13:40:14 +0200

    create a python class for perturbation

---
+++ perturbation_analysis_incl_gradCAM.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+import innvestigate
+import rasterio
+import numpy as np
+from rasterio.plot import reshape_as_image,reshape_as_raster
+import glob
+import geopandas as gpd
+from tensorflow.keras import models
+import matplotlib.pyplot as plt
+from skimage.measure import block_reduce
+tf.compat.v1.disable_eager_execution()
+
+class perturbation_analysis_incl_gradCAM:
+    
+    def __init__(self):
+        pass
+    
+    
+    
+    def run(self):
+        pass
+    
+
+if __name__ == "__main__":
+    pp = perturbation_analysis_incl_gradCAM()
+    pp.run()
