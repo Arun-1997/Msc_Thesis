eb225637fafaf38cb6c28d2c58cfd0db577f903d
Arun <a.v.venugopal@student.utwente.nl>
2023-05-15 13:54:55 +0200

    pertubate update

--- perturbation_analysis_incl_gradCAM.py
+++ perturbation_analysis_incl_gradCAM.py
@@ -13,8 +13,21 @@ tf.compat.v1.disable_eager_execution()
 class perturbation_analysis_incl_gradCAM:
     
     def __init__(self):
-        pass
-    
+        self.file_name = "Iowa_2021_july_8448-1792"
+        self.img_path = 'Input/sentinel/patches_256/Iowa_July_1_31/test/'+self.file_name+'.tif'
+        self.mask_model_id = "aanaxs4g" # With mask
+        self.nomask_model_id = "ezb3xkqf" # No Mask
+        self.mask_model_path = glob.glob("wandb/"+ "*"+self.mask_model_id+"*" + "/files/model-best.h5")[0]
+        self.nomask_model_path = glob.glob("wandb/"+ "*"+self.nomask_model_id+"*" + "/files/model-best.h5")[0]
+        self.mask_ev_gdf = gpd.read_file("Output/Evaluation/"+self.mask_model_id+".shp")
+        self.nomask_ev_gdf = gpd.read_file("Output/Evaluation/"+self.nomask_model_id+".shp")
+
+        # print(model_path)
+        self.mask_cnn_model = models.load_model(self.mask_model_path)
+        self.nomask_cnn_model = models.load_model(self.nomask_model_path)
+        self.mask_img_batch = np.expand_dims(img, axis=0)
+
+        self.nomask_img_batch = self.mask_img_batch1[:,:,:,0:12]
     
     
     def run(self):
